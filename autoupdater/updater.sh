7e5b1b4e20beb78319affa43a5a8c74ebef08dae
eric5949 <eric5949@outlook.com>
2026-04-25 11:32:17 -0400

    update

--- autoupdater/updater.sh
+++ autoupdater/updater.sh
@@ -7,11 +7,11 @@ mkdir -p ~/.local/share/tetosong
 
 mkdir -p ~/.local/share/tetosong/fortunes
 mkdir -p ~/.local/share/tetosong/fortunes/tetosotd
-curl -sLo ~/.local/share/tetosong/tetofortunes https://raw.githubusercontent.com/eric5949/tetosong/refs/heads/test/fortunes/tetosotd/tetofortunes
-curl -sLo ~/.local/share/tetosong/tetofortunes.dat https://raw.githubusercontent.com/eric5949/tetosong/refs/heads/test/fortunes/tetosotd/tetofortunes.dat
+curl -sLo ~/.local/share/tetosong/tetofortunes https://raw.githubusercontent.com/eric5949/tetosong/refs/heads/main/fortunes/tetosotd/tetofortunes
+curl -sLo ~/.local/share/tetosong/tetofortunes.dat https://raw.githubusercontent.com/eric5949/tetosong/refs/heads/main/fortunes/tetosotd/tetofortunes.dat
 AUDIO="$(. ~/.local/share/tetosong/tetosong.config; echo $AUDIO)"
 if [ "$AUDIO" = "YES" ]; then
-    curl -sLo ~/.local/share/tetosong/SOTD.zip https://raw.githubusercontent.com/eric5949/tetosong/refs/heads/test/audio/teto/SOTD.zip
+    curl -sLo ~/.local/share/tetosong/SOTD.zip https://raw.githubusercontent.com/eric5949/tetosong/refs/heads/main/audio/teto/SOTD.zip
     mkdir -p ~/.local/share/tetosong/audio/
     mkdir -p ~/.local/share/tetosong/audio/teto/
     unzip -o ~/.local/share/tetosong/SOTD.zip -d ~/.local/share/tetosong/audio/teto/
@@ -25,8 +25,8 @@ if [ "$AUTOUPDATE" = "YES" ]; then
     # write and enable systemd service file and timer user services
     echo "Auto-Updater enabled, updating service..."
     mkdir -p ~/.config/systemd/user
-    curl -sLo ~/.config/systemd/user/tetosong.service https://raw.githubusercontent.com/eric5949/tetosong/refs/heads/test/autoupdater/tetosong.service
-    curl -sLo ~/.config/systemd/user/tetosong.timer https://raw.githubusercontent.com/eric5949/tetosong/refs/heads/test/autoupdater/tetosong.timer
+    curl -sLo ~/.config/systemd/user/tetosong.service https://raw.githubusercontent.com/eric5949/tetosong/refs/heads/main/autoupdater/tetosong.service
+    curl -sLo ~/.config/systemd/user/tetosong.timer https://raw.githubusercontent.com/eric5949/tetosong/refs/heads/main/autoupdater/tetosong.timer
     systemctl --user daemon-reload
     systemctl --user enable tetosong.timer
     systemctl --user start tetosong.timer
@@ -36,6 +36,6 @@ fi
 # write tetosong to ~/.local/bin and tell the user how to use it.
 echo "writing tetosong to ~/.local/bin"
 mkdir -p ~/.local/bin
-curl -sLo ~/.local/bin/tetosong https://raw.githubusercontent.com/eric5949/tetosong/refs/heads/test/tetosong
+curl -sLo ~/.local/bin/tetosong https://raw.githubusercontent.com/eric5949/tetosong/refs/heads/main/tetosong
 chmod +x ~/.local/bin/tetosong
 echo "Update complete"
